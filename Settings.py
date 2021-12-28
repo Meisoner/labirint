5243fb77af3324ddff2da99a5684e78917ca8bee
Meisoner <58383223+Meisoner@users.noreply.github.com>
2021-12-28 21:16:52 +0400

    setting attributes in more pythonic way

--- Settings.py
+++ Settings.py
@@ -1,11 +1,17 @@
 class Settings:
     def __init__(self, file):
+        self.mdict = dict()
         with open(file) as f:
             for i in f.read().split('\n'):
                 s, arg = i.split()
                 if arg.isnumeric():
-                    setattr(self, s, int(arg))
+                    self.mdict[s] = int(arg)
                 elif arg.replace('.', '').isnumeric():
-                    setattr(self, s, float(arg))
+                    self.mdict[s] = float(arg)
                 else:
-                    setattr(self, s, arg)
+                    self.mdict[s] = arg
+
+    def __getattr__(self, k):
+        if k in self.mdict.keys():
+            return self.mdict[k]
+        return None
