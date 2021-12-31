0a0e0f5b2bdfa75aeae997cd7ad264308474819f
Meisoner <58383223+Meisoner@users.noreply.github.com>
2021-12-31 16:58:52 +0400

    I FIXED THEM I FIXED FIXED FIXED!!!!!!

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ keys = [pg.K_w, pg.K_s, pg.K_a, pg.K_d]
 block_image = pg.Surface((BLS, BLS))
 pg.draw.rect(block_image, (255, 0, 255), (0, 0, BLS, BLS))
 for i in range(2):
-    Block(objs[1], block_image, 2 + i, 4, 1)
+    Block(objs[1], block_image, 2, 4 + i, 1)
 xdist, ydist = 0, 0
 k = st.rays * BLS / (2 * math.tan(st.fov))
 while run:
@@ -47,10 +47,10 @@ while run:
             tg = sn / cs
             pc2 = [(i // BLS) * BLS for i in pc]
             sgn = [round(cs / abs(cs)), round(sn / abs(sn))]
-            pc2[0] += BLS * (sgn[0] + 1) // 2
-            pc2[1] += BLS * (sgn[1] + 1) // 2
+            pc2[0] += BLS * ((sgn[0] + 1) // 2)
+            pc2[1] += BLS * ((sgn[1] + 1) // 2)
             for _ in range(size[0] // BLS):
-                y = int(pc[1] + (pc2[0] - pc[0]) * tg)
+                y = pc[1] + (pc2[0] - pc[0]) * tg
                 for r in rects[i]:
                     if r[0] <= pc2[0] <= r[0] + BLS and r[1] <= y <= r[1] + BLS:
                         tobreak = True
@@ -58,12 +58,11 @@ while run:
                 if tobreak:
                     xdist = (pc2[0] - pc[0]) / cs
                     break
-                else:
-                    xdist = st.raylen + BLS
+                xdist = st.raylen + BLS
                 pc2[0] += sgn[0] * BLS
             tobreak = False
             for _ in range(size[1] // BLS):
-                x = int(pc[0] + (pc2[1] - pc[1]) / tg)
+                x = pc[0] + (pc2[1] - pc[1]) / tg
                 for r in rects[i]:
                     if r[0] <= x <= r[0] + BLS and r[1] <= pc2[1] <= r[1] + BLS:
                         tobreak = True
@@ -71,29 +70,24 @@ while run:
                 if tobreak:
                     ydist = (pc2[1] - pc[1]) / sn
                     break
-                else:
-                    ydist = st.raylen + BLS
+                ydist = st.raylen + BLS
                 pc2[1] += sgn[1] * BLS
             tobreak = False
             if xdist != st.raylen + BLS or ydist != st.raylen + BLS:
-                if xdist <= ydist:
-                    dists[i][rayn] = abs(round(xdist * optcos(angle - fov + rayn * step)))
-                else:
-                    dists[i][rayn] = abs(round(ydist * optcos(angle - fov + rayn * step)))
+                dists[i][rayn] = abs(round(min(xdist, ydist) * optcos(fov - rayn * step)))
             else:
                 dists[i][rayn] = 0
-            # print(dists[1])
             if dists[i][rayn]:
-                c = 255 / (1 + dists[i][rayn] ** 2 * 0.00002)
+                c = int(255 / (1 + dists[i][rayn] * 0.01))
                 hg = k / dists[i][rayn]
                 pg.draw.rect(layers[i], (c, c, c), (rayn * (size[0] // st.rays), size[1] // 2 - int(hg / 2), (size[0] // st.rays), int(hg)))
     screen.blit(layers[1], (0, 0))
     for i in range(4):
         if pressed[i]:
             if i < 2:
-                plr.update(0, tick * (2 * i - 1) / 5)
+                plr.update(0, tick * (2 * i - 1) / 10)
             else:
-                plr.update(tick * (2 * i - 5) / 5, 0)
+                plr.update(tick * (2 * i - 5) / 10, 0)
     pg.display.flip()
     for i in pg.event.get():
         if i.type == pg.QUIT:
