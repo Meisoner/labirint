a1135433f45adc9f8f3572302f941f5e543a904b
elizvoron <91742422+elizvoron@users.noreply.github.com>
2021-12-31 14:02:27 +0530

    Add files via upload

---
+++ Player.py
@@ -0,0 +1,24 @@
+import pygame as pg
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, blsize, size, group):
+        super().__init__(group)
+        self.image = pg.Surface((blsize, blsize))
+        pg.draw.rect(self.image, (100, 100, 100), (0, 0, 50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 0, 0
+        self.dx = self.dy = 0
+
+    def get_centre(self):
+        return (self.rect.x + self.rect.size[0] // 2, self.rect.y + self.rect.size[1] // 2)
+
+    def update(self, x, y):
+        self.dx += x
+        self.dy += y
+        if not (-1 < self.dx < 1):
+            self.rect.x += int(self.dx)
+            self.dx -= int(self.dx)
+        if not (-1 < self.dy < 1):
+            self.rect.y += int(self.dy)
+            self.dy -= int(self.dy)
