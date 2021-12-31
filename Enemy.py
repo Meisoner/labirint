bb8bdc30998b1903c459219ac846dc0b4fa9deb9
elizvoron <91742422+elizvoron@users.noreply.github.com>
2021-12-31 13:07:08 +0400

    Update Enemy.py

--- Enemy.py
+++ Enemy.py
@@ -6,7 +6,7 @@ class Enemy(pg.sprite.Sprite):
     def __init__(self, blsize, size, group):
         super().__init__(group)
         self.image = pg.Surface((blsize, blsize))
-        pg.draw.rect(self.image, (100, 100, 100), (0, 0, 50, 50))
+        pg.draw.rect(self.image, (0, 255, 0), (0, 0, 50, 50))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = random.randint(0, 1000), random.randint(0, 600)
         self.dx = self.dy = 0
