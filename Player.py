6828d7a557f5c464b7b2bcfc59a82a61e7dfcb96
elizvoron <91742422+elizvoron@users.noreply.github.com>
2022-01-06 00:53:23 +0400

    Update Player.py

--- Player.py
+++ Player.py
@@ -9,11 +9,14 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = 0, 0
         self.dx = self.dy = 0
+        self.life = 3
+        self.hp = 100
+        self.domage = 5
 
     def get_centre(self):
         return (self.rect.x + self.rect.size[0] // 2, self.rect.y + self.rect.size[1] // 2)
 
-    def update(self, x, y):
+    def update(self, x, y, all):
         self.dx += x
         self.dy += y
         if not (-1 < self.dx < 1):
@@ -22,3 +25,13 @@ class Player(pg.sprite.Sprite):
         if not (-1 < self.dy < 1):
             self.rect.y += int(self.dy)
             self.dy -= int(self.dy)
+        if pg.sprite.spritecollideany(self, all):
+            self.dx -= x
+            self.dy -= y
+
+    def hit(self):
+        return self.domage
+
+    def military(self, domage):
+        self.hp -= domage
+        return self.hp
