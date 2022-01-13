63166c273e3af17665931f8e1827df64b90cb0a4
elizvoron <91742422+elizvoron@users.noreply.github.com>
2022-01-13 23:03:26 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,100 @@ from Block import *
 from Utilites import *
 from Enemy import *
 import random
+from Start import *
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        # image = image.convert_alpha()
+        pass
+    return image
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Счастливых вам голодных игр))"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    f = True
+
+    while f:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+                f = False
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                f = False
+                return None
+        pygame.display.flip()
+
+
+def endd():
+    intro_text = ["ЖИЛИ БЫЛИ...", "", ""
+                  "НЕ ДОЖИЛИ..."]
+
+    fon = pygame.transform.scale(load_image('конец.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 50)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 100
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    f = True
+
+    while f:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+                f = False
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                exit()
+                return None
+        pygame.display.flip()
+
+
+
+
+
 
 BLS = 50
+
 
 
 pg.init()
 pg.mouse.set_visible(False)
 run, tobreak = True, False
-size = (1000, 600)
+size = WIDTH, HEIGHT = (1000, 600)
 screen = pg.display.set_mode(size)
 pg.display.set_caption('lol')
 layers = [pg.Surface(size) for _ in range(st.maxheight)]
@@ -37,6 +123,7 @@ fov = radians(st.fov)
 step = fov / st.rays
 angle = 0
 clock = pg.time.Clock()
+start_screen()
 pressed = [False] * 4
 keys = [pg.K_w, pg.K_a, pg.K_s, pg.K_d]
 block_image = pg.Surface((BLS, BLS))
@@ -148,6 +235,7 @@ while run:
         elif i.type == pg.KEYDOWN:
             if i.key == pg.K_ESCAPE:
                 run = False
+                endd()
             else:
                 for key in range(4):
                     if i.key == keys[key]:
@@ -163,3 +251,4 @@ while run:
             elif angle < 0:
                 angle = 2 * PI
             pg.mouse.set_pos(size[0] // 2, size[1] // 2)
+pg.quit()
